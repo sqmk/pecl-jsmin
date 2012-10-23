184e45b3cd69945b582395addefc1f9e8c20ac06
Michael Squires <sqmk@php.net>
2012-10-22 23:50:43 -0400

    Issue #11 - Updating email address, license, and semantic versioning.

--- php_jsmin.c
+++ php_jsmin.c
@@ -12,7 +12,7 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author: Michael Squires <squiresmk@gmail.com>                        |
+  | Author: Michael Squires <sqmk@php.net>                               |
   +----------------------------------------------------------------------+
 */
 
