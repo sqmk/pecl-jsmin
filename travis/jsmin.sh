9fe047d2a454428174b85087bf1a3cec98ecad1a
Michael Squires <sqmk@php.net>
2012-10-23 01:09:21 -0400

    Issue #12 - Use already checked out git project

--- travis/jsmin.sh
+++ travis/jsmin.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env sh
 
+# Create copy of project
+cp -R . project-copy
+
 # Get and extract PHP
 wget http://uk3.php.net/get/php-5.4.7.tar.bz2/from/this/mirror -O php-5.4.7.tar.bz2
 tar -xf php-5.4.7.tar.bz2
 cd php-5.4.7
 
 # Clone into ext directory
-git clone https://github.com/sqmk/pecl-jsmin.git ext/jsmin
+mv ../project-copy ext/jsmin
 
 # Build PHP
 ./buildconf --force
-./configure --disable-all --enable-debug --enable-jsmin
-make --quiet
+./configure --disable-all --enable-debug --enable-silent-rules --enable-jsmin
+make -s
 
 # Run tests
 TEST_PHP_EXECUTABLE=sapi/cli/php sapi/cli/php run-tests.php ext/jsmin  
