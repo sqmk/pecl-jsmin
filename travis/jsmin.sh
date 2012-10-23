7bb52bc4f3adce8cb8e08fbba21892754c921ed2
Michael Squires <sqmk@php.net>
2012-10-23 01:17:11 -0400

    Issue #12 - Change approach on copying files.

--- travis/jsmin.sh
+++ travis/jsmin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-# Create copy of project
-cp -R . project-copy
+# Move out of project 
+cd ../
 
 # Get and extract PHP
 wget http://uk3.php.net/get/php-5.4.7.tar.bz2/from/this/mirror -O php-5.4.7.tar.bz2
@@ -9,12 +9,12 @@ tar -xf php-5.4.7.tar.bz2
 cd php-5.4.7
 
 # Clone into ext directory
-mv ../project-copy ext/jsmin
+mv ../pecl-jsmin ext/jsmin
 
 # Build PHP
 ./buildconf --force
-./configure --disable-all --enable-debug --enable-silent-rules --enable-jsmin
-make -s
+./configure --disable-all --enable-debug --enable-jsmin
+make
 
 # Run tests
 TEST_PHP_EXECUTABLE=sapi/cli/php sapi/cli/php run-tests.php ext/jsmin  
